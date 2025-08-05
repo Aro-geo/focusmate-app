bd445d6227b08ffd95a1a1f1ca6e4f88bf77871d
Aro-geo <geokullo@gmail.com>
2025-08-05 09:04:56 +0300

    Updated auth-signup.js to be a proper async handler that calls the auth-register function.

--- api/auth-signup.js
+++ api/auth-signup.js
@@ -1,2 +1,8 @@
-// Redirect to auth-register for compatibility
-module.exports = require('./auth-register.js');
+const authRegister = require('./auth-register.js');
+
+async function handler(req, res) {
+  return authRegister(req, res);
+}
+
+module.exports = handler;
+module.exports.default = handler;
